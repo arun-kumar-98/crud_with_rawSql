606b0902ca49590d13fc7fe2005fd14d22c28925
arun-kumar-98 <arunrits15@gmail.com>
2022-12-23 10:15:10 +0530

    raw sql

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+const router = require('./src/router/router')
+const { connection } = require('./db')
+
+
+app.use(cors())
+app.use('/', router)
+
+
+function run() {
+    try {
+        connection.connect((err) => {
+            if (err) {
+                console.log('failled to connect to mysql database')
+            } else {
+                console.log('database connection successfull')
+            }
+        })
+
+        app.listen(process.env.port, () => {
+            console.log(`server listenig at port ${process.env.port}`)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+run()
---
+++ src/router/router.js
@@ -0,0 +1,15 @@
+const controller = require('../controller/nativeSQlQueryController')
+
+const router = require('express').Router()
+
+
+router.get('/create', controller.createSchema)
+router.post('/insert', controller.insertRecord)
+router.get('/all', controller.getAllRecord)
+router.put('/update', controller.updateRecord)
+router.delete('/delete/:id', controller.deleteParticularRecord)
+router.delete('/deleteAll', controller.deleteAllRecord)
+router.post('/bulk', controller.multipleRecordInsertion)
+
+
+module.exports = router
